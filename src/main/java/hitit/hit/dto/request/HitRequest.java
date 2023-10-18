6b044eb2f7826485988a9dc2854b043e08d165ab
Seun Yi <yiseun10@gmail.com>
2023-10-18 22:52:18 +0900

    :hammer: feat : hit increase DTO 추가

--- src/main/java/hitit/hit/dto/request/HitRequest.java
+++ src/main/java/hitit/hit/dto/request/HitRequest.java
@@ -1,4 +1,4 @@
-package hitit.hit;
+package hitit.hit.dto.request;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,18 +8,11 @@ import java.time.format.DateTimeFormatter;
 public class HitRequest {
 
     private Long urlId;
-    private LocalDateTime createdDate;
     public HitRequest(final Long urlId){
         this.urlId = urlId;
-        this.createdDate = LocalDateTime.now();
     }
     public Long getUrlId(){
         return this.urlId;
-    }
-
-    public LocalDateTime getCreatedDate(){
-        return createdDate;
-//        return createdDate.format(DateTimeFormatter.ofPattern("yyyy-mm-dd"));
     }
 
 }
