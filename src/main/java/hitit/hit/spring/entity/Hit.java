f62b5dd2984c2b7896fed21617eda0ccf82d0b52
Seun Yi <yiseun10@gmail.com>
2023-10-19 02:16:49 +0900

    :hammer: feat : getTotalHit 메서드 추가

--- src/main/java/hitit/hit/spring/entity/Hit.java
+++ src/main/java/hitit/hit/spring/entity/Hit.java
@@ -1,5 +1,6 @@
 package hitit.hit.spring.entity;
 
+import hitit.hit.dto.response.TotalHitResponse;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class Hit {
         return this.getHit();
     }
 
-    //
+    public Long getTotalHit(){return this.getTotalHit();}
     public Hit(Long urlId,Long hit,Long totalHit){
         this.urlId = urlId;
         this.hit = hit;
@@ -38,5 +39,7 @@ public class Hit {
         return new Hit(urlId,0L, 0L);
     }
     public static Hit initHit(Hit hit){return new Hit(hit.urlId,0L,hit.totalHit);}
+
+
 
 }
