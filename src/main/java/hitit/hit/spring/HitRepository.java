4934e7a0dae40155faa4454d17a17f87072ee632
Seun Yi <yiseun10@gmail.com>
2023-10-20 13:21:10 +0900

    :hammer: refactor : Hit Repository 리팩토링

--- src/main/java/hitit/hit/spring/HitRepository.java
+++ src/main/java/hitit/hit/spring/HitRepository.java
@@ -1,9 +1,14 @@
 package hitit.hit.spring;
 
+import hitit.hit.dto.response.HitResponse;
 import hitit.hit.entity.Hit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
+
+    Optional<Hit> findById(Long urlId);
 }
